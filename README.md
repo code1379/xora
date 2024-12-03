0861f007c8b3c0c3fcf0dcb0ea767df4e3f555dc
code1379 <coderxialuo@gmail.com>
2024-12-04 01:57:19 +0800

    feat: Header 组件的编写

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 ## [React 中使用 tailwindcss](https://tailwindcss.com/docs/guides/vite)
 
 [添加 snippets](https://github.com/adrianhajdin/xora?tab=readme-ov-file#snippets)
+
+## 媒体查询
+
+```
+屏幕大于 1024px
+lg:hidden
+
+屏幕小于 1024px
+max-lg:hidden
+```
---
+++ src/sections/Header.jsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import { Link as LinkScroll } from "react-scroll";
+import { clsx } from "clsx";
+
+const NavLink = ({ title }) => (
+  <LinkScroll className="base-bold text-p4 uppercase trransition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+    {title}
+  </LinkScroll>
+);
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+      <div className="container flex h-14 items-center max-lg:px-5">
+        {/* 
+          大于 1024
+          lg:hidden 屏幕宽度大于 1024 px 会隐藏掉 
+        */}
+        <a className="lg:hidden flex-1 cursor-pointer z-2">
+          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+        </a>
+
+        {/* 我们想让小屏的时候点击 右侧，下面展示成菜单 */}
+        {/* 
+          小于 1024 
+          max-lg:fiexed 屏幕宽度小于 1024 px 会 fixed
+          作用等同于 max-width: 1024px 
+        */}
+        <div
+          className={clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
+          )}
+        >
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before">
+            {/* 导航条 */}
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                {/* 左侧 */}
+                <li className="nav-li">
+                  <NavLink title="features" />
+                  <div className="dot" />
+                  <NavLink title="pricing" />
+                </li>
+                {/* 中间 logo */}
+                <li className="nav-logo">
+                  <LinkScroll
+                    to="hero"
+                    offset={-100}
+                    spy
+                    smooth
+                    className={clsx(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
+                    )}
+                  >
+                    <img
+                      src="/images/xora.svg"
+                      width={160}
+                      height={55}
+                      alt="logo"
+                    />
+                  </LinkScroll>
+                </li>
+                {/* 右侧 */}
+                <li className="nav-li">
+                  <NavLink title="faq" />
+                  <div className="dot" />
+                  <NavLink title="download" />
+                </li>
+              </ul>
+            </nav>
+            {/* 导航背景 */}
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90 ">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="outline"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* 小屏幕展示button */}
+        <button
+          className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
+          onClick={() => setIsOpen((prevState) => !prevState)}
+        >
+          <img
+            src={`/images/${isOpen ? "close" : "magic"}.svg`}
+            alt="magic"
+            className="size-1/2 object-contain"
+          />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
